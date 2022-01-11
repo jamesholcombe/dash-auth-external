86dba0144d889184032404c619f8cc59bc493b01
jamesholcombe <jholcombe@hotmail.co.uk>
2022-01-11 22:28:03 +0000

    update setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
 REQUIRES = ["requests", "requests_oauthlib", "flask"]
 
 NAME = "dash-auth-external"
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name=NAME,
-    version="0.2.2",
+    version="0.2.3",
     description="Integrate Dash with 3rd Parties and external providers",
     author_email="jholcombe@hotmail.co.uk",
     url="https://github.com/jamesholcombe/dash-auth-external",
@@ -14,7 +18,5 @@ setup(
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    long_description="""\
-    Integrate Plotly Dash with 3rd Parties and external providers."
-    """,
+    long_description=long_description,
 )
